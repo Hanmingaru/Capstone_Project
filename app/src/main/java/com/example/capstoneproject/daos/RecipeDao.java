ef45486d284610e8a90ca56292680409eadd573b
erheault <erheault@vt.edu>
2022-03-30 19:31:03 -0400

    Changed database scheme

--- app/src/main/java/com/example/capstoneproject/daos/RecipeDao.java
+++ app/src/main/java/com/example/capstoneproject/daos/RecipeDao.java
@@ -1,4 +1,25 @@
 package com.example.capstoneproject.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Query;
+
+import com.example.capstoneproject.entities.Recipe;
+
+import java.util.List;
+
+@Dao
 public interface RecipeDao {
+
+    @Query("SELECT * FROM recipe")
+    List<Recipe> getAll();
+
+    @Query("SELECT * FROM recipe WHERE uid IN (:recipeIds)")
+    List<Recipe> loadAllByIds(int[] recipeIds);
+
+    @Query("SELECT * FROM recipe WHERE name LIKE :name LIMIT 1")
+    Recipe findByName(String name);
+
+    @Delete
+    void delete(Recipe recipe);
 }
---
+++ app/src/main/java/com/example/capstoneproject/database/RecipeDB.java
@@ -0,0 +1,9 @@
+package com.example.capstoneproject.database;
+
+import androidx.room.Database;
+import com.example.capstoneproject.entities.Recipe;
+
+@Database(version=1, entities = {Recipe.class})
+public class RecipeDB {
+//    abstract fun
+}
