b4c06139d8c35ddb2dcb9485e8f5f8e7a1eb70ef
erheault <erheault@vt.edu>
2022-03-31 13:49:22 -0400

    Changed database scheme

--- app/src/main/java/com/example/capstoneproject/database/RecipeDB.java
+++ app/src/main/java/com/example/capstoneproject/database/RecipeDB.java
@@ -1,9 +1,12 @@
 package com.example.capstoneproject.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.capstoneproject.daos.RecipeDao;
 import com.example.capstoneproject.entities.Recipe;
 
 @Database(version=1, entities = {Recipe.class})
-public class RecipeDB {
-//    abstract fun
+public abstract class RecipeDB extends RoomDatabase {
+    public abstract RecipeDao recipeDao();
 }
