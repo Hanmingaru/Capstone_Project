6fe0fe9465f59878f2ba7842bc4c27e359cc7fba
hwanchoi510 <hwanchoi510@yahoo.com>
2022-04-29 08:32:33 -0400

    UI changes that was mentioned during meeting

--- app/src/main/java/com/example/capstoneproject/SplashScreen.java
+++ app/src/main/java/com/example/capstoneproject/SplashScreen.java
@@ -1,28 +1,14 @@
-/*
- * Created by Elliott Rheault on 2022.4.9
- * Copyright © 2022 Elliott Rheault. All rights reserved.
- */
-
 package com.example.capstoneproject;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class SplashScreen extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen.this, SwipeActivity.class));
-                finish();
-            }
-        }, 1000);
+        setContentView(R.layout.activity_splash_screen);
     }
-
 }
